b2856dbdc2d8dd87fc4ee6e4594e07e2edc40069
Error-free IT <michael@errorfreeit.com.au>
2015-05-21 22:02:36 +0930

    Added setsidebarfavorites.sh

---
+++ setsidebarfavorites.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Author: Michael Page (21/05/15)
+
+# Usage
+# Install changesidebarlist.pkg: https://github.com/Error-freeIT/Change-Sidebar-List/releases/latest
+# Install outset.pkg: https://github.com/chilcote/outset/releases/latest
+# Copy this script into /usr/local/outset/login-once/
+
+# List of intended sidebar favourites.
+FAVORITES=( "/Applications" 
+		"HOMEDIR/Desktop"
+		"HOMEDIR/Documents"
+		"HOMEDIR/Downloads"
+		"HOMEDIR/Movies"
+		"HOMEDIR/Music"
+		"HOMEDIR/Pictures"
+		"HOMEDIR" )
+
+
+# Special sidebar favorites we skip removing.
+IGNORED_FAVORITES=( "All My Files"
+			"iCloud"
+			"AirDrop" )
+
+
+### Nothing below this line needs to change. ####
+
+CHANGE_SIDEBAR_LIST_SCRIPT="/usr/local/bin/changesidebarlist"
+
+# Create an array of current favorites.
+IFS=$'\n'
+CURRENT_FAVORITES=($($CHANGE_SIDEBAR_LIST_SCRIPT list | sed 's/file:.*//' | sed 's/ *$//g'))
+unset IFS
+
+# Remove all unwanted favorites apart from those in the ignored favorties array.
+for CURRENT_FAVORITE in "${CURRENT_FAVORITES[@]:1}"
+do
+
+	IGNORED="false"
+
+	# Loop through the array of ignored favorties.
+	for IGNORED_FAVORITE in "${IGNORED_FAVORITES[@]}"
+	do
+		# If current favorite is an ignored favortie, set IGNORED to true.	
+		if [[ "$IGNORED_FAVORITE" == "$CURRENT_FAVORITE" ]]
+		then
+			IGNORED="true"
+		fi
+	done
+
+	if [[ "$IGNORED" == "false" ]]
+	then
+		$CHANGE_SIDEBAR_LIST_SCRIPT remove "${CURRENT_FAVORITE}"
+	fi
+
+done
+
+# Add wanted favorites.
+for FAVORITE in "${FAVORITES[@]}"
+do
+	$CHANGE_SIDEBAR_LIST_SCRIPT last "$FAVORITE"
+done
+
+exit 0
